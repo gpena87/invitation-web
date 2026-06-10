fdc2c9aa309e7b3294673c9ff2dcece59271a65b
gpena87 <gpenagutierrez87@gmail.com>
2026-06-09 21:45:41 -0400

    fix

--- src/app/pages/home/home-page.component.ts
+++ src/app/pages/home/home-page.component.ts
@@ -1,12 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { UserMusicComponent } from "../../components/user-music/user-music.component";
 import { UserFormComponent } from '../../components/user-form/user-form.component';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [UserMusicComponent, UserFormComponent],
+  imports: [UserMusicComponent, UserFormComponent, HeaderComponent],
 })
 export default class HomePage {}
