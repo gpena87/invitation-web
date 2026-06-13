1591f3e5aa88895762d7fc0ba303a816efab51cf
gpena87 <gpenagutierrez87@gmail.com>
2026-06-13 17:38:03 -0400

    fix

--- src/app/components/start/start.component.ts
+++ src/app/components/start/start.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-start',
   imports: [],
   templateUrl: './start.component.html',
 })
-export class StartComponent {}
+export class StartComponent {
+  title = signal('Invitacion de boda');
+  subtitle = signal('María José y Gonzalo');
+  subtitle2 = signal('Estamos emocionados de compartir este momento tan especial con ustedes. ¡Gracias por ser parte de nuestra historia!');
+}
